55d0e1d86bcf125dbe284fe128352ca6d9602245
vikhang73vn <91653588+vikhang73vn@users.noreply.github.com>
2023-05-08 10:58:41 +0700

    Update BookRepository.java

--- src/main/java/com/erp/backend/repositories/BookRepository.java
+++ src/main/java/com/erp/backend/repositories/BookRepository.java
@@ -7,22 +7,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 8c2fcc47fc3037cedb2aa38575b5590b725daf17
-import java.util.Optional;
->>>>>>> dev
 
-
-<<<<<<< HEAD
-<<<<<<< HEAD
 public interface BookRepository extends JpaRepository<Book, Long> {
-    List<Book> findByBookNameContainingIgnoreCaseOrAuthorNameContainingIgnoreCase(String name, String author);
+   // List<Book> findByBookNameContainingIgnoreCaseOrAuthorNameContainingIgnoreCase(String name, String author);
 
-}
-=======
+
+
     @Query("select b from Book b where b.bookId=?1")
     public Book getBookById(long id);
     @Query("select  b from Book  b where b.category.category_id = ?1")
@@ -58,17 +48,11 @@ public interface BookRepository extends JpaRepository<Book, Long> {
 //
 ////public interface BookRepository extends CrudRepository<Book, Long> {
 ////}
->>>>>>> dev
-=======
-    @Query("select b from Book b where b.bookId=?1")
-    public Book getBookById(long id);
-    @Query("select  b from Book  b where b.category.category_id = ?1")
-    public List<Book> getBookByCategory(long id);
-    @Query("select  b from Book  b where b.author.author_id = ?1")
-    public List<Book> getBookByAuthor(long id);
 
-}
+
+
+
 
 //public interface BookRepository extends CrudRepository<Book, Long> {
 //}
->>>>>>> 8c2fcc47fc3037cedb2aa38575b5590b725daf17
+
