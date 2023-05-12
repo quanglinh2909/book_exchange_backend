975d0f8a6ae22d7f5db1bb03b2da1add9d18bdf7
NguyenHuuQuan-19130182 <91517270+NguyenHuuQuan-19130182@users.noreply.github.com>
2023-05-12 13:43:39 +0700

    no message

---
+++ src/main/java/com/erp/backend/dtos/auth/BookDto.java
@@ -0,0 +1,16 @@
+package com.erp.backend.dtos.auth;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+@Data
+public class BookDto {
+    @NotBlank(message = "Nhap ten sach")
+    private String bookName;
+    private String bookDescribe;
+    @NotNull(message = "Nhap danh muc")
+    private Long category;
+    @NotNull(message = "Nhap tac gia")
+    private Long author;
+}
