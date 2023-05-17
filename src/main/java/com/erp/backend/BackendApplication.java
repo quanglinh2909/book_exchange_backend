258118c08d14ccd9042915ccc604af47d9f93df5
QUANGLINH <hackcoquanglinh2000@gmail.com>
2023-05-17 14:26:48 +0700

    sdafsaf

--- src/main/java/com/erp/backend/BackendApplication.java
+++ src/main/java/com/erp/backend/BackendApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendApplication {
+	//hello
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
